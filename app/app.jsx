9801568f75bd58f028188a82b015f397c80842e5
Vina Compson <vv.compson@icloud.com>
2018-04-22 19:56:13 +1000

    initial commit

--- app/app.jsx
+++ app/app.jsx
@@ -3,6 +3,7 @@ var ReactDOM = require('react-dom');
 var { Route, Router, IndexRoute, hashHistory } = require('react-router');
 
 
+var TodoApp = require('TodoApp');
 
 //Load foundation
 $(document).foundation();
@@ -11,6 +12,6 @@ $(document).foundation();
 require('style!css!sass!applicationStyles');
 
 ReactDOM.render(
-  <p>Boilerplate 3 Project</p>,
+  <TodoApp />,
   document.getElementById("app")
 );
