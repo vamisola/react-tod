0adb72a540372e2c909f87607dca616dbdb4e86f
Vina Compson <vv.compson@icloud.com>
2018-04-24 19:56:51 +1000

    add filter functionality

--- app/api/TodoAPI.jsx
+++ app/api/TodoAPI.jsx
@@ -16,14 +16,37 @@ module.exports = {
     } catch (e) {
 
     }
-
     return $.isArray(todos) ? todos : [];
-
     //same as the ternary above
     // if ($.isArray(todos)) {
     //   return todos;
     // } else {
     //   return [];
     // }
+  },
+  filterTodos: function (todos, showCompleted, searchText) {
+    var filteredTodos = todos;
+
+    //Filter by showCompleted
+    filteredTodos = filteredTodos.filter((todo) => {
+      return !todo.completed || showCompleted;
+    });
+    //Filter by searchText
+    filteredTodos = filteredTodos.filter((todo) => {
+      var text = todo.text.toLowerCase();
+      return searchText.length === 0 || text.indexOf(searchText) > -1;
+    });
+    //Sort todos with non-completed first
+    filteredTodos.sort((a, b) => {
+      if(!a.completed && b.completed){
+        return -1;
+      } else if(a.completed && !b.completed){
+        return 1;
+      }else{
+        return 0;
+      }
+    });
+    return filteredTodos;
   }
+
 };
